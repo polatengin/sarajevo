acfea3a722ff7eabd553e869a27d008e2503ca55
polatengin <enpolat@microsoft.com>
2021-03-02 17:05:52 +0300

    app service plan create script added

--- .iac/infra.sh
+++ .iac/infra.sh
@@ -2,3 +2,5 @@ PROJECT_NAME="sarajevo"
 LOCATION="westeurope"
 
 az group create --name "${PROJECT_NAME}-rg" --location "${LOCATION}"
+
+az appservice plan create --resource-group "${PROJECT_NAME}-rg" --name "${PROJECT_NAME}-plan" --location "${LOCATION}" --sku "FREE"
