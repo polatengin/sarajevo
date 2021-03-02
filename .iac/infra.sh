42ffc235d5b55b1781f3dcaaea1d69a42847e95b
polatengin <enpolat@microsoft.com>
2021-03-02 17:06:00 +0300

    web app create script added

--- .iac/infra.sh
+++ .iac/infra.sh
@@ -4,3 +4,5 @@ LOCATION="westeurope"
 az group create --name "${PROJECT_NAME}-rg" --location "${LOCATION}"
 
 az appservice plan create --resource-group "${PROJECT_NAME}-rg" --name "${PROJECT_NAME}-plan" --location "${LOCATION}" --sku "FREE"
+
+az webapp create --resource-group "${PROJECT_NAME}-rg" --plan "${PROJECT_NAME}-plan" --name "${PROJECT_NAME}-web"
